c35ff89b8a1d648889f1a96228c51bcb9fc5ebc7
Liam <ixausliam@gmail.com>
2018-06-23 21:13:37 +1000

    Animation change to my journey so far, queries change for smaller screen to resize resources and portfolio. Added javascript icon to footer.

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -3,13 +3,49 @@ $(document).ready(function() {
 $('.js--wp-1').waypoint(function(direction) {
         $('.js--wp-1').addClass('animated fadeIn');
 }, {
-        offset:'25%'  
+        offset:'70%'  
 });
 
 $('.js--wp-2').waypoint(function(direction) {
-        $('.js--wp-2').addClass('animated bounceInLeft');
+        $('.js--wp-2').addClass('animated fadeIn');
 }, {
         offset:'60%'  
+});
+
+$('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+}, {
+        offset:'60%'  
+});
+
+$('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated fadeIn');
+}, {
+        offset:'60%'  
+});
+
+$('.js--wp-5').waypoint(function(direction) {
+        $('.js--wp-5').addClass('animated fadeIn');
+}, {
+        offset:'60%'  
+});
+
+$('.js--wp-6').waypoint(function(direction) {
+        $('.js--wp-6').addClass('animated fadeIn');
+}, {
+        offset:'60%'  
+});
+
+$('.js--wp-7').waypoint(function(direction) {
+        $('.js--wp-7').addClass('animated fadeIn');
+}, {
+        offset:'60%'  
+});
+
+$('.js--wp-8').waypoint(function(direction) {
+        $('.js--wp-8').addClass('animated bounceInLeft');
+}, {
+        offset:'65%'  
 });
 
 });
